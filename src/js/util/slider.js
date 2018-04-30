ded149cc41a56104a33d25862febae9bea043322
Yanagida Ayaka <rainymoment0616@gmail.com>
2018-04-30 17:32:53 +0900

    Swiper.jsいれた！（動いたー！！）

--- src/js/util/slider.js
+++ src/js/util/slider.js
@@ -1,5 +1,5 @@
 import slick from 'slick-carousel/slick/slick.min'
-import swiper from 'swiper/dist/js/swiper'
+import Swiper from 'swiper/dist/js/swiper'
 
 export default class SliderFunc {
     constructor(target){
@@ -10,17 +10,87 @@ export default class SliderFunc {
             $slider: $('[data-target-slider]')
         };
         if(!target.$slider.length) return;
-        target.$slider.swiper({
-            loop: true,
-            pagination: '.swiper-pagination'
+        // console.log('swiper');
+
+        target.$slider.map(i => {
+            console.log($(target.$slider[i]).data('target-slider'));
+            let swiperConfig = {};
+            switch ($(target.$slider[i]).data('target-slider')){
+                case 'custom01':
+                    swiperConfig = {
+                        loop: true,
+                        direction: 'vertical',
+                        speed: 1000,
+                        height: 200,
+                        autoHeight: true,
+                        pagination: {
+                            el: '.swiper-pagination',
+                            type: 'bullets'
+                        },
+                        breakpoints: {
+                            800: {
+                                height: 100
+                            }
+                        },
+                        autoplay: {
+                            delay: 4000
+                        }
+                    };
+                    break;
+                case 'custom02':
+                    swiperConfig = {
+                        loop: true,
+                        slidesPerView: 3,
+                        breakpoints: {
+                            400: {
+                                slidesPerView: 1
+                            },
+                            800: {
+                                slidesPerView: 2
+                            }
+                        },
+                        speed: 1000,
+                        navigation: {
+                            nextEl: '.swiper-button-next',
+                            prevEl: '.swiper-button-prev'
+                        },
+                        pagination: {
+                            el: '.swiper-pagination',
+                            type: 'bullets'
+                        },
+                        autoplay: {
+                            delay: 4000
+                        }
+                    };
+                    break;
+                case 'default':
+                default:
+                    swiperConfig = {
+                        loop: true,
+                        speed: 1000,
+                        navigation: {
+                            nextEl: '.swiper-button-next',
+                            prevEl: '.swiper-button-prev'
+                        },
+                        pagination: {
+                            el: '.swiper-pagination',
+                            type: 'bullets'
+                        },
+                        autoplay: {
+                            delay: 4000
+                        }
+                    };
+                    break;
+            }
+            const mySwiper = new Swiper (target.$slider[i], swiperConfig);
         })
-        // console.log('test');
     }
     slickSlider(){
         const target = {
             $slider: $('[data-target-slick]')
         };
         if(!target.$slider.length) return;
+        console.log('slick');
         target.$slider.slick({
             dots: true,
             autoplay: true,
